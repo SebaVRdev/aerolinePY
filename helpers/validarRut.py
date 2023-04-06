75f5d1d4ae0de82d3b55d3566135676f698abdc4
SebaVRdev <sebarm177@gmail.com>
2023-04-06 16:41:47 -0400

    fix: :bug: CHG Validar rut fue mejorado en base a su estructura

--- helpers/validarRut.py
+++ helpers/validarRut.py
@@ -5,9 +5,9 @@ from itertools import cycle
 
 def digito_verificador(rut): 
     split_rut = rut.split("-")
-    rut_num   = split_rut[0]            #OBTENEMOS solo los numeros del RUT
-    dv        = split_rut[1].upper()    #OBTENEMOS el digito verificador que ingreso el usuario lo convertimos en MAYUSCULA
-
+    rut_num   = split_rut[0]             #OBTENEMOS solo los numeros del RUT
+    dv        = split_rut[1].upper()     #OBTENEMOS el digito verificador que ingreso el usuario lo convertimos en MAYUSCULA
+    
     # CALCULAMOS el digito verificador que deberia tener el RUT
     reversed_digits = map(int, reversed(str(rut_num)))
     factors         = cycle(range(2, 8))
@@ -21,6 +21,6 @@ def digito_verificador(rut):
        dv_calculated  = 'K'
 
     #RETORNAMOS un booleano, TRUE <- en caso de que el dv calculado sea igual al dv original que mando el usuario
-    return dv == dv_calculated
+    return dv == str(dv_calculated)
 
-print(digito_verificador('20790208-k'))
+#print(digito_verificador('20790208-k'))
